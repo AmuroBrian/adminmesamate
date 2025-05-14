310f7746bdeff75591f31fa210e30afa2f6c4778
unknown <jerem3392@gmail.com>
2025-05-15 04:41:44 +0800

    sidebars

--- script/mysqldb.js
+++ script/mysqldb.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: '192.168.100.90',   // XAMPP MySQL host
+    host: '192.168.100.26',   // XAMPP MySQL host
     port: 3307,
     user: 'root',        // Default MySQL user
     password: '',        // No password for root (leave empty)
